5ee0b7bb0b9300a089f9b309249736c72115401f
Aaron Thakoordeen <aaronthakoordeen@gmail.com>
2024-01-16 11:12:22 -0500

    .history folder rm

--- .history/context/active-section-context_20240116104438.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import { links } from "@/lib/data";
-
-export default function ActiveSectionContextProvider() {
-  const [activeSection, setActiveSection] = useState<">("Home");
-
-  return <div>ActiveSectionContextProvider</div>;
-}
