94cb9e2096a824958725a7d9469fe402d72b558f
Aaron Thakoordeen <aaronthakoordeen@gmail.com>
2024-01-16 14:04:01 -0500

    style: ADD contact section

--- components/experience.tsx
+++ components/experience.tsx
@@ -14,8 +14,9 @@ import { useSectionInView } from "@/lib/hooks";
 import TimelineElement from "./timeline-element";
 
 export default function Experience() {
+  const { ref } = useSectionInView("Experience");
   return (
-    <section id="experience">
+    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
